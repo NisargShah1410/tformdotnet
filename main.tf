3a2b4b8225ffdbdf8425180e8f63261af2b0a0a1
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-25 16:08:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-hello-azure-tf"
+    resource_group_name  = "rg-demoazure-tf"
     storage_account_name = "nisargazuretf"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
 }
 
-resource "azurerm_resource_group" "rg-hello-azure" {
-  name     = "rg-hello-azure"
+resource "azurerm_resource_group" "rg-demoazure" {
+  name     = "rg-demoazure"
   location = "northcentralus"
 }
