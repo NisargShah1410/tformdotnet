d0621d278d1610976b9cc43d4374e2369ad42d4b
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-26 13:25:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,29 @@ provider "azurerm" {
 }
 
 terraform {
-  backend "azurerm" {
+  backend "remote" {
+    organization = "nisarg1410"
     subscription_id      = "c9e520b1-2096-473b-973b-e2d45ac13eae"
     resource_group_name  = "rg-demoazure-tf"
     storage_account_name = "nisargazuretf"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
+
+    workspaces {
+      name = "gh-action-demo"
+    }
   }
 }
+
+#terraform {
+ # backend "azurerm" {
+  #  subscription_id      = "c9e520b1-2096-473b-973b-e2d45ac13eae"
+   # resource_group_name  = "rg-demoazure-tf"
+    #storage_account_name = "nisargazuretf"
+    #container_name       = "terraform-state"
+    #key                  = "terraform.tfstate"
+  #}
+#}
 
 resource "azurerm_resource_group" "rg-demoazure" {
   name     = "rg-demoazure"
